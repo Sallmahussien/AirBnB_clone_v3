9c747852dba0c07fb9895df15e4d72df4e0ae592
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-10-30 00:43:43 +0200

    update cities api again

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -20,7 +20,7 @@ def get_city_by_state_id(state_id):
 
     city_list = [city.to_dict() for city in state_by_id.cities]
 
-    return jsonify(city_list), 200
+    return jsonify(city_list)
 
 
 @app_views.route('/cities/<city_id>', methods=['GET'])
@@ -35,21 +35,21 @@ def get_city(city_id):
 
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'])
-def delete(city_id):
+def delete_city(city_id):
     """Deletes a city with specific id"""
     city_by_id: City = storage.get(City, city_id)
 
     if not city_by_id:
         abort(404)
 
-    storage.delete(city_by_id)
+    city_by_id.delete()
     storage.save()
 
     return jsonify({}), 200
 
 
 @app_views.route('/states/<state_id>/cities', methods=['POST'])
-def post(state_id):
+def create_city(state_id):
     """Creates new city"""
     state_by_id: State = storage.get(State, state_id)
 
@@ -62,15 +62,14 @@ def post(state_id):
     if "name" not in body_request.keys():
         abort(400, "Missing name")
 
-    city = City(name=body_request.get('name'), state_id=state_by_id.id)
-    storage.new(city)
-    storage.save()
+    city = City(name=body_request.get('name'), state_id=state_id)
+    city.save()
 
     return jsonify(city.to_dict()), 201
 
 
 @app_views.route('/cities/<city_id>', methods=['PUT'])
-def update(city_id):
+def update_city(city_id):
     """Update an existing city"""
     city_by_id: City = storage.get(City, city_id)
 
@@ -82,7 +81,9 @@ def update(city_id):
     if not body_request:
         abort(400, "Not a JSON")
 
-    city_by_id.name = body_request.get('name', city_by_id.name)
-    storage.save()
+    for key, value in request.get_json().items():
+        if key not in ['id', 'state_id', 'created_at', 'updated_at']:
+            setattr(city_by_id, key, value)
+    city_by_id.save()
 
     return jsonify(city_by_id.to_dict()), 200
