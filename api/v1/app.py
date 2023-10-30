5c3bd109beddb042b57c332e088d0a24906b76d7
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-10-30 06:36:40 +0200

    allow HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,12 +6,14 @@ from api.v1.views import app_views
 from flask import Flask, make_response, jsonify
 from models import storage
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
 
 app.url_map.strict_slashes = False
 
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(404)
