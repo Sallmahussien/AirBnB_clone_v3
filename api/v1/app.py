826b7ec8cb30b652f11373b4bfbb3752d26c3970
salma hussien <salmaa.hussien1@gmail.com>
2023-10-30 02:42:30 +0200

    Remove CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,6 @@
 from api.v1.views import app_views
 
 from flask import Flask, make_response, jsonify
-from flask_cors import CORS
 
 from models import storage
 from os import getenv
@@ -14,8 +13,6 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 app.register_blueprint(app_views)
-
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(404)
