758b92f48b8eefcd13acb5df7d4448616c247c3c
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-10-30 16:31:01 +0200

    Add places amenities get method

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """Implement places_amenities view"""
 
-from api.v1.views import app_views
 from flask import abort, jsonify
 
+from api.v1.views import app_views
 from models import storage
-from models.place import Place, place_amenity
-from models.amenity import Amenity
+from models.place import Place
 
 
 @app_views.route('/places/<place_id>/amenities', methods=['GET'])
@@ -20,45 +19,3 @@ def get_amenities_by_place_id(place_id):
     amenities_list = [amenity.to_dict() for amenity in place_by_id.amenities]
 
     return jsonify(amenities_list), 200
-
-
-@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['DELETE'])
-def delete_amenities(place_id, amenity_id):
-    """Deletes an Amenity object to a Place"""
-    place_by_id = storage.get(Place, place_id)
-
-    if not place_by_id:
-        abort(404)
-
-    amenity_by_id = storage.get(Amenity, amenity_id)
-    if not amenity_by_id:
-        abort(404)
-
-    if amenity_by_id not in place_by_id.amenities:
-        abort(404)
-
-    amenity_by_id.delete()
-    storage.save()
-
-    return jsonify({}), 200
-
-
-@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'])
-def link_amenity_to_place(place_id, amenity_id):
-    """Link an Amenity object to a Place"""
-    place_by_id = storage.get(Place, place_id)
-
-    if not place_by_id:
-        abort(404)
-
-    amenity_by_id = storage.get(Amenity, amenity_id)
-    if not amenity_by_id:
-        abort(404)
-
-    if amenity_by_id in place_by_id.amenities:
-        return jsonify(amenity_by_id), 200
-
-    place_by_id.amenities.append(amenity_by_id)
-    place_by_id.save()
-
-    return jsonify(amenity_by_id), 201
