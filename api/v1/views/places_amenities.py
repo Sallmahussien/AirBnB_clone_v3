8df7b3a885cbd4b85ca772e963c599b5c532a475
salma hussien <salmaa.hussien1@gmail.com>
2023-10-30 23:26:12 +0200

    Implement link places_amenities method

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -47,3 +47,28 @@ def delete_amenties(place_id, amenity_id):
     place_by_id.save()
 
     return jsonify({}), 200
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'])
+def link_amenity_to_place(place_id, amenity_id):
+    """Link a Amenity object to a Place"""
+    place_by_id = storage.get(Place, place_id)
+    if not place_by_id:
+        abort(404)
+
+    amenity_by_id = storage.get(Amenity, amenity_id)
+    if not amenity_by_id:
+        abort(404)
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        amenities_by_place = place_by_id.amenities
+    else:
+        amenities_by_place = place_by_id.amenity_ids
+
+    if amenity_by_id in amenities_by_place:
+        return jsonify(amenity_by_id.to_dict()), 200
+
+    amenities_by_place.append(amenity_by_id.to_dict())
+    place_by_id.save()
+
+    return jsonify(amenity_by_id), 201
